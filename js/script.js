cbfe6aa20e94e36986aca7b2dbc2c9143e204c8c
dnuzum <derek.nuzum@gmail.com>
2016-04-25 11:03:07 -0700

    need to fix commits

--- js/script.js
+++ js/script.js
@@ -1,49 +1,42 @@
-$(document).ready(function(){
-	var images = []
-	var interval = -1
-	var index = 0
+$(document).ready(function() {
+	var images = [];
+	var interval = -1;
+	var index = 0;
 
  	function getImages(subject) {
  		var url = "http://www.reddit.com/search.json?q="+subject+"nsfw:no";
- 		$.get(url, {
- 			}).done(function(data){
- 			 images = data.data.children.map(function(child){
+ 		$.get(url, function(data) {
+ 			 images = data.data.children.map(function(child) {
  				return child.data;
  				}).filter(function(child) {
  					return child.preview != undefined;
  				}).map(function(child) {
- 					return child.preview.images[0].source.url
- 				})
- 				console.log(images);
- 			});
- 			var filtered = searchResult.filter(function (filter){
- 				return filter !== undefined;
- 			});
-
-	getImages("cat");
-
+ 					return child.preview.images[0].source.url;
+ 				});
+ 				startSlideshow();
+ 		});
+	}
 	function startSlideshow() {
 		if (interval != -1) {
 			stopSlideshow();
 		}
+		interval = setInterval(function() {
+			var imageHolder = $(".input-group-button");
+			imageHolder.html("<img data-src="+images[index++]+' alt="Pictures go here!" src='+images[index++]+"/>");
+			index = index % images.length;
+		}, 2000)
 	}
-	interval = setInterval(function() {
-		var imageHolder = $(".input-group-button");
-		imageHolder.html("");
-		imageHolder.html("<img data-src="+images[index++]+' alt="Pictures go here!" src='+images[index++]+"/>");
-		index = index % images.length;
-	}, 2000)
+
 	function stopSlideshow() {
 		clearInterval(interval);
-		index = 0;
+		index = -1;
 	}
-	
+
 		$('#searchBtn').on('click', function(e){
  		e.preventDefault();
  		var input = $('.search').val();
 				$('.search').val('');
 		stopSlideshow();
-		getImages(toSearch);
-	})
-	}
+		getImages(input);
+		});
 });
